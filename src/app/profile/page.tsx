2133154a4314caf61f86c12ac16636d4d6b2be29
sabeern <sabeerneyyan@gmail.com>
2023-08-13 11:48:38 +0530

    Added logout in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("api/users/logout");
+      toast.success("Logout successfull", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      router.push("/login");
+    } catch (err) {
+      toast.error("Logout failed", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  };
   return (
     <div>
       <h1>Profile page</h1>
+      <div className="flex justify-end mr-4">
+        <button
+          className="bg-blue-500 text-white px-2 py-1 rounded-xl"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
